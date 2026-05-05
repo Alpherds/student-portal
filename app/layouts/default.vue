98b2b0d5f1dcea6f681561b0a871ad2aaa292508
unknown <sirraivensupan@gmail.com>
2026-05-05 09:57:27 +0800

    layouts setup using navigation drawer

---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+        <v-list>
+            <v-list-item 
+                prepend-icon="mdi-view-dashboard" 
+                title="Home" 
+                value="home"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                    <v-row>
+                        <v-col>
+                            <v-text-field
+                               label="Last Name"
+                               variant="outlined"
+                               placeholder="Dela Cruz"
+                             ></v-text-field>
+                        </v-col>
+                    </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
