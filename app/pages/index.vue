757144aa6156c068264ca1f503f35f4060658988
unknown <sirraivensupan@gmail.com>
2026-05-19 09:31:58 +0800

    layouts and pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,15 +2,59 @@
     <v-app>
         <v-main>
             <v-container>
-                    <v-row>
-                        <v-col>
-                            <v-text-field
-                               label="Last Name"
-                               variant="outlined"
-                               placeholder="Dela Cruz"
-                             ></v-text-field>
-                        </v-col>
-                    </v-row>
+                <v-row>
+                    <v-col
+                        cols="12"
+                    >
+                        <v-text-field
+                         label="Student Number"
+                         variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Last Name"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="First Name"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col
+                        col="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Middle Name"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-combobox
+                            label="Course"
+                            :items="['BSCS', 'DCT']"
+                            variant="outlined"
+                        ></v-combobox>
+                    </v-col>
+
+
+                </v-row>  
             </v-container>
         </v-main>
     </v-app>
